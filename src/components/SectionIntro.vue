d56561a73667ccf03e9fa6160d4fc1fc0d76f622
serii <seriiburduja@gmail.com>
2021-02-14 22:26:07 +0200

    servizi page

---
+++ src/components/SectionIntro.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="section__intro">
+    <img :src="image" alt="">
+    <h2 class="section__title" v-html="title"></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['image', 'title']
+}
+</script>
+
+<style lang="scss">
+.section__intro {
+  position: relative;
+  margin-bottom: 33px;
+  height: 730px;
+  background-size: cover;
+  background-position: left center;
+  background-repeat: no-repeat;
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    z-index: 1;
+  }
+}
+@media screen and (max-width: 968px) {
+  .section__intro {
+    height: 400px;
+  }
+}
+.section__title {
+  position: absolute;
+  bottom: 60px;
+  left: 85px;
+  font-size: 30px;
+  font-weight: bold;
+  z-index: 2;
+}
+@media screen and (max-width: 601px) {
+  .section__title {
+    left: 30px;
+  }
+}
+</style>
--- src/pages/Servizi.vue
+++ src/pages/Servizi.vue
@@ -1,11 +1,25 @@
 <template>
   <Layout>
-    <h1>Servizi</h1>
+    <SectionIntro :title="$page.wordPressPage.content" :image="$page.wordPressPage.featuredMedia.sourceUrl"/>
+    <Servizi/>
   </Layout>
 </template>
-
+<page-query>
+query { wordPressPage(id: "106") { title content featuredMedia { sourceUrl } } }
+</page-query>
 <script>
-export default {}
+import SectionIntro from "../components/SectionIntro";
+import Servizi from "../components/Servizi";
+
+export default {
+  components: {
+    SectionIntro,
+    Servizi
+  },
+  mounted () {
+    console.log(this.$page.wordPressPage.content);
+  }
+}
 </script>
 
 <style scoped>
