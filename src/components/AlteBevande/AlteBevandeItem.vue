994c897126273289f398b8355d99c2ce96c64714
serii <seriiburduja@gmail.com>
2021-02-15 16:34:54 +0200

    ALte bevande transition

--- src/components/AlteBevande/AlteBevandeItem.vue
+++ src/components/AlteBevande/AlteBevandeItem.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="alte-bevande__item " :class="classList">
-    <a target="_blank" rel="noreferrer" :href="url" class="alte-bevande__img">
-      <img :src="image" width="430" height="432" alt="" class="aligncenter">
-      <span></span> <span></span> <span></span> <span></span> </a>
-    <h3 class="title">{{ title }}</h3>
-    <a class="link" target="_blank" rel="noreferrer" :href="url"> <span> Visita il sito</span> </a>
-  </div>
+  <transition name="fade" appear>
+    <div class="alte-bevande__item " :class="classList">
+      <a target="_blank" rel="noreferrer" :href="url" class="alte-bevande__img">
+        <img :src="image" width="430" height="432" alt="" class="aligncenter">
+        <span></span> <span></span> <span></span> <span></span> </a>
+      <h3 class="title">{{ title }}</h3>
+      <a class="link" target="_blank" rel="noreferrer" :href="url"> <span> Visita il sito</span> </a>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -25,5 +27,10 @@ export default {
 </script>
 
 <style lang="scss">
-
+.fade-enter-active {
+  transition: opacity 1s;
+}
+.fade-enter {
+  opacity: 0;
+}
 </style>
